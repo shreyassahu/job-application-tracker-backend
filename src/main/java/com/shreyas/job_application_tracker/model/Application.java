c9a02f58ebf5e1538cacdd8cf06f4c8e05c89934
Shreyas Sahu <sahu@Shreyass-MacBook-Pro.local>
2025-12-16 18:47:49 -0500

    Created the Application entity

--- src/main/java/com/shreyas/job_application_tracker/model/Application.java
+++ src/main/java/com/shreyas/job_application_tracker/model/Application.java
@@ -1,4 +1,43 @@
 package com.shreyas.job_application_tracker.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "applications")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Application {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String company;
+
+    @Column(nullable = false)
+    private String position;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ApplicationStatus status;
+
+    @Column(nullable = false)
+    private LocalDate appliedDate;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 }
